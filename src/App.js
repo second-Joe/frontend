a27050b11f3681e054ea45b91bf8e27a5430e31c
dbtmdtt <dbtmdtt@gmail.com>
2023-03-08 16:53:40 +0900

    스크롤 효과 줬습니다. movie_project홈에서 똑같은 효과를 줬는데 안먹히던데 계속 찾아보겠습니다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from "react";
 import axios from "axios";
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const App = () => {
   const [genres, setGenres] = useState("");
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const scrollRef = useRef(null);
+  const [isDrag, setIsDrag] = useState(false);
+  const [startX, setStartX] = useState();
+
+  const onDragStart = (e) => {
+    e.preventDefault();
+    setIsDrag(true);
+    setStartX(e.pageX + scrollRef.current.scrollLeft);
+  };
+
+  const onDragEnd = () => {
+    setIsDrag(false);
+  };
+
+  const onDragMove = (e) => {
+    if (isDrag) {
+      const { scrollWidth, clientWidth, scrollLeft } = scrollRef.current;
+
+      scrollRef.current.scrollLeft = startX - e.pageX;
+
+      if (scrollLeft === 0) {
+        setStartX(e.pageX);
+      } else if (scrollWidth <= clientWidth + scrollLeft) {
+        setStartX(e.pageX + scrollLeft);
+      }
+    }
+  };
+  const throttle = (func, ms) => {
+    let throttled = false;
+    return (...args) => {
+      if (!throttled) {
+        throttled = true;
+        setTimeout(() => {
+          func(...args);
+          throttled = false;
+        }, ms);
+      }
+    };
+  };
+  const delay = 100;
+  const onThrottleDragMove = throttle(onDragMove, delay);
 
   const getMovies = () => {
     axios
@@ -29,55 +70,52 @@ const App = () => {
     setSelectedMovie(null);
   };
   return (
-    <>
-      {/* <header
-        className="banner"
-        style={{
-          backgroundSize: "cover",
-          backgroundImage: `url({movie.background_image})`,
-          backgroundPosition: "center center",
-        }}
-      ></header> */}
-      <main className="app-container">
-        <h2>장르</h2>
-        <div className="movie-grid">
-          {movies.map((movie) => (
-            <div
-              key={movie.id}
-              className="movie-item"
-              onClick={() => handleMovieClick(movie)}
-            >
-              <img
-                className="movie_image"
-                src={movie.medium_cover_image}
-                alt={movie.title}
-              />
-              <div className="movie-overlay">
-                <div className="movie-title">{movie.title}</div>
-              </div>
-            </div>
-          ))}
-        </div>
-        {selectedMovie && (
-          <div className="modal">
-            <div className="modal-content">
-              <img
-                className="modal-image"
-                src={selectedMovie.large_cover_image}
-                alt={selectedMovie.title}
-              />
-              <div className="modal-details">
-                <h2 className="modal-title">{selectedMovie.title}</h2>
-                <p className="modal-summary">{selectedMovie.summary}</p>
-                <button className="close-button" onClick={handleCloseClick}>
-                  Close
-                </button>
-              </div>
+    <main className="app-container">
+      <h2>장르</h2>
+      <div
+        className="movie-grid"
+        onMouseDown={onDragStart}
+        onMouseMove={isDrag ? onThrottleDragMove : null}
+        onMouseUp={onDragEnd}
+        onMouseLeave={onDragEnd}
+        ref={scrollRef}
+      >
+        {movies.map((movie) => (
+          <div
+            key={movie.id}
+            className="movie-item"
+            onClick={() => handleMovieClick(movie)}
+          >
+            <img
+              className="movie_image"
+              src={movie.medium_cover_image}
+              alt={movie.title}
+            />
+            <div className="movie-overlay">
+              <div className="movie-title">{movie.title}</div>
             </div>
           </div>
-        )}
-      </main>
-    </>
+        ))}
+      </div>
+      {selectedMovie && (
+        <div className="modal">
+          <div className="modal-content">
+            <img
+              className="modal-image"
+              src={selectedMovie.large_cover_image}
+              alt={selectedMovie.title}
+            />
+            <div className="modal-details">
+              <h2 className="modal-title">{selectedMovie.title}</h2>
+              <p className="modal-summary">{selectedMovie.summary}</p>
+              <button className="close-button" onClick={handleCloseClick}>
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </main>
   );
 };
 export default App;
--- src/Row.js
+++ src/Row.js
@@ -1,34 +1,54 @@
-import "./Row.css";
-import React from "react";
-import imgLogo from "./post.jpg";
-import {
-  ArrowBackIosOutlined,
-  ArrowForwardIosOutlined,
-} from "@material-ui/icons";
+// import React from "react";
+// import axios from "axios";
+// import "./App.css";
+// import { useState, useEffect } from "react";
 
-function Row() {
-  return (
-    <div className="row">
-      <h2>장르</h2>
-      <ArrowBackIosOutlined className="sliderArrow left" />
-      <div className="row__posters">
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-        <img className="poster" src={imgLogo} />
-      </div>
-      <ArrowForwardIosOutlined className="sliderArrow right" />
-    </div>
-  );
-}
+// const Row = ({ setGenres }) => {
+//   const [genres, setGenres] = useState("");
+//   const [movies, setMovies] = useState([]);
+//   const [selectedMovie, setSelectedMovie] = useState(null);
+//   const getMovies = () => {
+//     axios
+//       .get(`https://yts.mx/api/v2/list_movies.json?genres=${genres}`)
+//       .then((response) => {
+//         setMovies(response.data.data.movies);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+//   useEffect(() => {
+//     getMovies();
+//   });
+//   const handleMovieClick = (movie) => {
+//     setSelectedMovie(movie);
+//   };
 
-export default Row;
+//   const handleCloseClick = () => {
+//     setSelectedMovie(null);
+//   };
+//   return (
+//     <main className="app-container">
+//       <h2>장르</h2>
+//       <div className="movie-grid">
+//         {movies.map((movie) => (
+//           <div
+//             key={movie.id}
+//             className="movie-item"
+//             onClick={() => handleMovieClick(movie)}
+//           >
+//             <img
+//               className="movie_image"
+//               src={movie.medium_cover_image}
+//               alt={movie.title}
+//             />
+//             <div className="movie-overlay">
+//               <div className="movie-title">{movie.title}</div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </main>
+//   );
+// };
+// export default Row;
