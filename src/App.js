148e5d1be2c2cabfdf988ed1200f0c7df265eeca
dbtmdtt <dbtmdtt@gmail.com>
2023-03-08 14:25:53 +0900

    지훈님 코드로 변경했습니다.

--- src/App.js
+++ src/App.js
@@ -21,31 +21,61 @@ const App = () => {
   useEffect(() => {
     getMovies();
   });
+  const handleMovieClick = (movie) => {
+    setSelectedMovie(movie);
+  };
 
+  const handleCloseClick = () => {
+    setSelectedMovie(null);
+  };
   return (
     <>
-      <header
+      {/* <header
         className="banner"
         style={{
           backgroundSize: "cover",
           backgroundImage: `url({movie.background_image})`,
           backgroundPosition: "center center",
         }}
-      ></header>
+      ></header> */}
       <main className="app-container">
         <h2>장르</h2>
         <div className="movie-grid">
           {movies.map((movie) => (
-            <div key={movie.id}>
+            <div
+              key={movie.id}
+              className="movie-item"
+              onClick={() => handleMovieClick(movie)}
+            >
               <img
                 className="movie_image"
                 src={movie.medium_cover_image}
                 alt={movie.title}
               />
-              <div className="movie-title">{movie.title}</div>
+              <div className="movie-overlay">
+                <div className="movie-title">{movie.title}</div>
+              </div>
             </div>
           ))}
         </div>
+        {selectedMovie && (
+          <div className="modal">
+            <div className="modal-content">
+              <img
+                className="modal-image"
+                src={selectedMovie.large_cover_image}
+                alt={selectedMovie.title}
+              />
+              <div className="modal-details">
+                <h2 className="modal-title">{selectedMovie.title}</h2>
+                <p className="modal-summary">{selectedMovie.summary}</p>
+                <button className="close-button" onClick={handleCloseClick}>
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </main>
     </>
   );
