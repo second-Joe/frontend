689012b0ecfac6d560aec332cab8b3e6b4a9368e
Hoooniya <gnsdl7274@gmail.com>
2023-03-10 01:17:28 +0900

    footer 작업

--- movie_project/src/components/Footer.js
+++ movie_project/src/components/Footer.js
@@ -1,8 +1,17 @@
 const Footer = () => {
   return (
-    <div>
-      <div>footer</div>
-    </div>
+    <footer>
+      <ul style={{ display: 'flex', color: 'white', listStyle: 'none',  justifyContent: 'center'}}>
+        <li style={{ margin: '0 1.5rem' }}>2조</li>
+        <li style={{ margin: '0 1.5rem' }}>김다희</li>
+        <li style={{ margin: '0 1.5rem' }}>김병우</li>
+        <li style={{ margin: '0 1.5rem' }}>김지훈</li>
+        <li style={{ margin: '0 1.5rem' }}>남승인</li>
+        <li style={{ margin: '0 1.5rem' }}>신승주</li>
+        <li style={{ margin: '0 1.5rem' }}>이길주</li>
+        <li style={{ margin: '0 1.5rem' }}>전유승</li>
+      </ul>
+    </footer>
   );
 };
 
