1f7d51c760dac1e363b6e4a1c2c58a0e63f34376
Hoooniya <gnsdl7274@gmail.com>
2023-03-10 01:44:49 +0900

    footer 사이즈 변경

--- movie_project/src/components/Footer.js
+++ movie_project/src/components/Footer.js
@@ -1,15 +1,22 @@
 const Footer = () => {
   return (
     <footer>
-      <ul style={{ display: 'flex', color: 'white', listStyle: 'none',  justifyContent: 'center'}}>
-        <li style={{ margin: '0 1.5rem' }}>2조</li>
-        <li style={{ margin: '0 1.5rem' }}>김다희</li>
-        <li style={{ margin: '0 1.5rem' }}>김병우</li>
-        <li style={{ margin: '0 1.5rem' }}>김지훈</li>
-        <li style={{ margin: '0 1.5rem' }}>남승인</li>
-        <li style={{ margin: '0 1.5rem' }}>신승주</li>
-        <li style={{ margin: '0 1.5rem' }}>이길주</li>
-        <li style={{ margin: '0 1.5rem' }}>전유승</li>
+      <ul
+        style={{
+          display: "flex",
+          color: "#858080",
+          listStyle: "none",
+          justifyContent: "center",
+          fontSize: "13px",
+        }}
+      >
+        <li style={{ margin: "0 1.5rem" }}>2조</li>
+        <li style={{ margin: "0 1.5rem" }}>김다희</li>
+        <li style={{ margin: "0 1.5rem" }}>김병우</li>
+        <li style={{ margin: "0 1.5rem" }}>김지훈</li>
+        <li style={{ margin: "0 1.5rem" }}>남승인</li>
+        <li style={{ margin: "0 1.5rem" }}>이길주</li>
+        <li style={{ margin: "0 1.5rem" }}>전유승</li>
       </ul>
     </footer>
   );
