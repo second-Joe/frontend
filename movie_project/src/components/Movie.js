9a7e3f829037f5eeb415316d882bedff4e607c48
GilJuLee <dlrlfwn95@gmail.com>
2023-03-09 15:18:24 +0900

    주석지움

--- movie_project/src/components/Movie.js
+++ movie_project/src/components/Movie.js
@@ -1,11 +1,9 @@
-import "./Movie.css"
 import Modal from '@mui/material/Modal';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import "./Movie.css"
 function Movie({ id, medium_cover_image, title, summary, genres }) {
-    //모달창 스타일
+
     const style = {
         position: 'absolute',
         top: '50%',
@@ -20,33 +18,28 @@ function Movie({ id, medium_cover_image, title, summary, genres }) {
         display: "flex",
         zIndex: 9999,
     };
-    // 마우스 호버 확인
+
     const [isHover, setIsHover] = React.useState(false);
     const handleHover = () => setIsHover(true);
     const handleLeave = () => setIsHover(false);
 
-    //모달창 열림/닫힘
     const [open, setOpen] = React.useState(false);
     const handleOpen = (e) => {
         e.stopPropagation();
         setOpen(true);
         setIsHover(false);
-        //클릭하자마자 마우스 호버가 되서 적용 불가. 클릭시에도 큰모달과 작은모달이 동시 존재
     }
-    //모달창 닫히게 하기 위함
     const handleClose = () => setOpen(false);
 
 
 
     return (
         <div
-            //반응형으로 하기 위함
             style={{
                 position: "relative",
                 width: "100%",
                 height: "100%",
             }}
-            //마우스 호버를 재정의
             onMouseEnter={handleHover}
             onMouseLeave={handleLeave}
         >
@@ -54,56 +47,34 @@ function Movie({ id, medium_cover_image, title, summary, genres }) {
                 src={medium_cover_image}
                 title={title}
                 style={{
-                    //반응형으로 하기 위함
                     width: "100%",
                     height: "100%",
-                    //object-fit: 이미지와 같은 요소를 지정된 너비와 높이로 지정하기 위함
-                    //cover: 종횡비를 유지하면서 정의된 너비와 높이를 가득 채울때까지 확대
                     objectFit: "cover",
-                    //transition : 전환을 구현
-                    //all : 여기서 움직이는 모든 속성을
-                    //0.3s ease out : 시작은 0.3초만에, 끝날떄는 좀 더 느리게 
                     transition: "all 0.3s ease-out",
-                    //호버가 되면 이미지 크기를 1.1배로 늘림
                     transform: isHover ? "scale(1.1)" : "scale(1)",
-                    //커서가 모양을 바꿈
                     cursor: "pointer",
                 }}
-                //클릭시 모달창이 오픈됨
                 onClick={handleOpen}
             />
-            {/* 마우스 호버시 제목과, 검은 미니모달 */}
             {isHover ? (
-                //온클릭을 설정한 이유는 미니모달을 클릭시
-                //큰 모달창이 열리지 않아서 따로 모달창을 열리게 설정
                 <div onClick={handleOpen}
                     style={{
                         position: "absolute",
                         top: 0,
                         left: 0,
-                        //반응형으로 설정하기위해 높이와 너비를 %로 설정
                         width: "100%",
                         height: "100%",
-                        //다양한 작업을 가능하게 하기 위해 flex를 사용
-                        //예)컨테이너 안에 있는 아이템 요소들을 수평 방향으로 배치하거나, 
-                        //간격을 조정하거나, 요소의 크기를 동적으로 변경하거나, 
-                        //아이템의 순서를 변경
                         display: "flex",
-                        //justifyContent: "center"는 글의 좌우간격을 가운데로 설정
                         justifyContent: "center",
-                        //alignItems: "center"는 글의 높이를 가운데로 설정
                         alignItems: "center",
                         zIndex: 10,
-                        // animation: isHover ? "none" : "$fadeInOut 2s ease-out",
                         backgroundColor: "rgba(0,0,0,0.8)",
                         cursor: "pointer",
                     }}
                 >
                     <Typography
-                        variant="h6" //variant: 글의 크기 설정
-                        component="span" //콘솔Warning: validateDOMNesting제거하기 위해 설정함
-                        //Typograpy는 <p>형식인데 이를 변경하기 위함.
-                        //여기서는 큰 이유는 없음. 밑의 Typography가 중요함
+                        variant="h6"
+                        component="span"
                         sx={{
                             color: "white",
                             p: 2,
@@ -113,12 +84,8 @@ function Movie({ id, medium_cover_image, title, summary, genres }) {
                         {title}
                     </Typography>
                 </div>
-                //호버 시 행동 끝. 호버중이 아니라면 NULL
             ) : null}
-            {/* 모달창 시작 */}
             <Modal
-                //keepMounted가 없을 시. 이미지 클릭시 작은모달이 계속 떠 있는 상태가
-                //있는 경우가 있음. 그걸 방지하기 위함
                 keepMounted
                 open={open}
                 onClose={handleClose}
@@ -135,15 +102,10 @@ function Movie({ id, medium_cover_image, title, summary, genres }) {
                         ></img>
                         <Typography
                             component="span"
-                            //콘솔Warning: validateDOMNesting제거하기 위해 설정함
-                            //Typography는 <p>와 같은 요소인데, 안에 <div>가 있을 시
-                            //Warning: validateDOMNesting이 출력된다.
                             sx={{
                                 color: "white",
                             }}
-
                             style={{
-                                //marginLeft를 주지 않는다면, 글이 이미지 밑으로 가게 됨
                                 marginLeft: "16px",
                                 display: "flex",
                                 width: "450px",
@@ -152,29 +114,17 @@ function Movie({ id, medium_cover_image, title, summary, genres }) {
                             <div
                                 style={{
                                     display: "block",
-                                    //flexDirection: "column":h2 요소와 p 요소가 수직 방향으로 배치되게 하는
-                                    //요소로, Typography 컴포넌트 내부에서 
-                                    //display: flex 속성을 사용하여 flex-direction: column 스타일을 적용
                                     flexDirection: "column",
-
-                                    // 현재 whitespace를 불가능하게 만든 이유는. 한줄로 만 출력되기 때문
-                                    // whiteSpace: "nowrap",
                                 }}
                             >
                                 <h2 style={{ display: "block" }}>{title}</h2>
                                 <p
                                     style={{
-                                        //현재는 textoverflow 밑 기타 요소들을, 밑의 삼항연산자가 대체
                                         fontSize: 18,
                                         display: "block",
                                         overflow: "hidden"
-                                        //textOverflow: 글자가 maxHeight를 넘을 시, 보이지 않게 하기 위함
-                                        //overflow hidden으로는 일정 높이를 초과하게 되면 나머지 부분을 숨김
-                                        // textOverflow: "ellipsis",
-                                        // maxHeight: "320px",
                                     }}
                                 >
-                                    {/* 글자가 600자가 넘을시, 600글자에서 짜르고 ...을 나오게 함 */}
                                     {summary.length > 500
                                         ? `${summary.slice(0, 500)}...`
                                         : summary}
--- movie_project/src/pages/SearchResult.js
+++ movie_project/src/pages/SearchResult.js
@@ -1,35 +1,23 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
-import Grid from "@mui/material/Grid"; // Grid version 1
+import Grid from "@mui/material/Grid"; 
 
-// 검색어 scarch를 받아와서
 const SearchResult = ({ search }) => {
   const [loading, setLoding] = useState(true);
   const [movies, setMovies] = useState([]);
-  //우선 콘솔에 입력이 됨
-  console.log(search);
-  //그 후 searchMovies에서, 해당 검색어에 관련된 영화 리스트를 불러오는 api를 가져온다.
-  //fetch함수는 첫번째 인자로 URL, 두번째 인자로 옵션 객체를 받고, Promise 타입의 객체를 반환
-  //그 후 서버로부터 받아온 검색 결과를 
-  //.json()이라는 함수를 통해 fetch()에서 가져온 데이터를 const json에 저장함 
+ 
+ 
   const searchMovies = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/list_movies.json?limit=50&query_term=${search}`)
     ).json();
-    //await가 두번 쓰인 이유 : fetch(url) 함수로부터 반환된 Promise 객체가 처리될 때까지 기다린 이후(첫번째 await)
-    //Promise 객체가 처리되면 Response 객체가 반환(서버로부터 받아온 HTTP 응답에 대한 정보같은 이상한데이터들이 있음)
-    //response 객체의 데이터를 적절하게 이용하기 위해 .json()메소드를 사용
-    //.json() 메서드를 호출하여, 서버로부터 받아온 JSON 데이터를 자바스크립트 객체로 변환하는 
-    //Promise 객체가 생성, 이 Promise 객체가 처리될 때까지 기다린 이후(두번째 await)
-    //JSON 데이터를 자바스크립트 객체로 변환한 결과를 반환
     setMovies(json.data);
     setLoding(false);
-    console.log("json ====", json);
   };
   useEffect(() => {
     searchMovies();
   }, [search]);
-  //search가 변경될 때 마다 리렌더링되어 함수가 실행됨
+ 
   return (
     <div>
       {loading ? (
