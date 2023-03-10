a4f516d535c3e7caa5aba62fa06b54f0f804a8f5
Hoooniya <gnsdl7274@gmail.com>
2023-03-10 14:44:49 +0900

    style : 화면 아래로 더

--- movie_project/src/components/Footer.js
+++ movie_project/src/components/Footer.js
@@ -1,8 +1,25 @@
 const Footer = () => {
   return (
-    <div>
-      <div>footer</div>
-    </div>
+    <footer>
+      <ul
+        style={{
+          display: "flex",
+          color: "#858080",
+          listStyle: "none",
+          justifyContent: "center",
+          fontSize: "13px",
+          marginTop: "50px"
+        }}
+      >
+        <li style={{ margin: "0 1.5rem" }}>2조</li>
+        <li style={{ margin: "0 1.5rem" }}>김다희</li>
+        <li style={{ margin: "0 1.5rem" }}>김병우</li>
+        <li style={{ margin: "0 1.5rem" }}>김지훈</li>
+        <li style={{ margin: "0 1.5rem" }}>남승인</li>
+        <li style={{ margin: "0 1.5rem" }}>이길주</li>
+        <li style={{ margin: "0 1.5rem" }}>전유승</li>
+      </ul>
+    </footer>
   );
 };
 
--- movie_project/src/components/LayOut.js
+++ movie_project/src/components/LayOut.js
@@ -3,56 +3,27 @@ import NavBar from "./NavBar";
 import Footer from "./Footer";
 import { useState } from "react";
 import SearchResult from "../pages/SearchResult";
-import Rating8 from "./rating8";
-import Rating85 from "./rating8.5";
-import Rating9 from "./rating9";
 
 const Layout = () => {
   const [search, setSearch] = useState("");
-  const [menuClick, setMenuClick] = useState(false);
-  const [menuKind, setMenuKind] = useState("");
 
-  const menuName = menuKind;
-  //console.log(menuName);
   return (
-    <div>
-      {/* search input에서 글자 입력하면  main 안 section1, section2, section3도 보여주면서 검색결과 바뀌어서 보여줌(section3), 엔터치면 section3으로 전체 화면 대체 */}
-      <div>
-        <header>
-          <NavBar
-            search={search}
-            setSearch={setSearch}
-            menuClick={menuClick}
-            setMenuClick={setMenuClick}
-            menuKind={menuKind}
-            setMenuKind={setMenuKind}
-          />
-        </header>
-        <main>
-          {menuClick === false ? (
-            search === "" ? (
-              <div>
-                <Outlet />
-                <footer>
-                  <Footer />
-                </footer>
-              </div>
-            ) : (
-              <SearchResult search={search} />
-            )
-          ) : search === "" ? (
-            menuName === "평점 8" ? (
-              <Rating8 />
-            ) : menuName === "평점 8.5" ? (
-              <Rating85 />
-            ) : (
-              <Rating9 />
-            )
-          ) : (
-            <SearchResult search={search} />
-          )}
-        </main>
-      </div>
+    <div
+      style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+    >
+      <header>
+        <NavBar search={search} setSearch={setSearch} />
+      </header>
+      <main>
+        {search === "" ? (
+          <div>
+            <Outlet />
+          </div>
+        ) : (
+          <SearchResult search={search} />
+        )}
+      </main>
+      <Footer />
     </div>
   );
 };
