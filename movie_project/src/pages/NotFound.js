6672eb583ed1dbe0f096d4e045475b5f25f4e3b9
diana815 <diana815@naver.com>
2023-03-10 14:30:35 +0900

    style : notFound page 스타일 변경

--- movie_project/src/pages/NotFound.js
+++ movie_project/src/pages/NotFound.js
@@ -1,7 +1,38 @@
 const NotFound = () => {
+  const errormsg1 = `I'm sorry :(`;
+  const errormsg2 = `We can't find the page!`;
   return (
-    <div>
-      <div>NotFound page</div>
+    <div
+      style={{
+        display: "flex",
+        margin: "0 auto",
+        justifyContent: "center",
+        marginTop: "100px",
+        color: "white",
+        fontWeight: "bold",
+        fontSize: "3em",
+      }}
+    >
+      <div
+        id="errormsg"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "100px",
+          color: "white",
+          fontWeight: "bold",
+          fontSize: "2em",
+        }}
+      >
+        <div style={{ display: "block", width: "100%", textAlign: "center" }}>
+          {errormsg1}{" "}
+        </div>
+        <div style={{ display: "block", width: "100%", textAlign: "center" }}>
+          {errormsg2}{" "}
+        </div>
+      </div>
     </div>
   );
 };
