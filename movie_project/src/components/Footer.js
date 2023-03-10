dd6a063fc7f04723f11c51f605ec6ab6be25ab7f
Hoooniya <gnsdl7274@gmail.com>
2023-03-10 15:05:52 +0900

    feat : 페이지 하단에 top 버튼 만들어서 누르면 페이지 위로 올라감.

--- movie_project/src/components/Footer.js
+++ movie_project/src/components/Footer.js
@@ -1,4 +1,22 @@
+import { useState } from "react";
+
 const Footer = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  const handleScroll = () => {
+    if (window.pageYOffset > 200) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
   return (
     <footer>
       <ul
@@ -8,7 +26,8 @@ const Footer = () => {
           listStyle: "none",
           justifyContent: "center",
           fontSize: "13px",
-          marginTop: "50px"
+          marginTop: "50px",
+          position: "relative"
         }}
       >
         <li style={{ margin: "0 1.5rem" }}>2조</li>
@@ -18,6 +37,25 @@ const Footer = () => {
         <li style={{ margin: "0 1.5rem" }}>남승인</li>
         <li style={{ margin: "0 1.5rem" }}>이길주</li>
         <li style={{ margin: "0 1.5rem" }}>전유승</li>
+        {showButton && (
+          <button
+            style={{
+              position: "absolute",
+              bottom: "1rem",
+              right: "3rem",
+              backgroundColor: "#e6e6e6",
+              color: "#333",
+              padding: "0.5rem 1rem",
+              borderRadius: "5px",
+              border: "none",
+              outline: "none",
+              cursor: "pointer"
+            }}
+            onClick={handleClick}
+          >
+            Top
+          </button>
+        )}
       </ul>
     </footer>
   );
