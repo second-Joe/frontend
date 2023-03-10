c63daafd1e2fdaa30e4043bb41a4308c8d4f0a70
GilJuLee <dlrlfwn95@gmail.com>
2023-03-10 10:10:55 +0900

    feat : 화살표 버튼  반응형 기능 추가 fix : 각 버튼 클릭 시 투명 박스로 클릭시 화면이 크게 변하는 현상 수정

--- movie_project/src/components/OutputMovieGenres.js
+++ movie_project/src/components/OutputMovieGenres.js
@@ -4,6 +4,7 @@ import Movie from "../components/Movie";
 import IconButton from '@mui/material/IconButton';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { Box } from "@mui/material";
 
 const OutputMovieGenres = ({ genre }) => {
     const [movies, setMovies] = useState([]);
@@ -36,7 +37,10 @@ const OutputMovieGenres = ({ genre }) => {
 
     return (<div>
         {loading ?
-            (<div> <h1>&nbsp;</h1></div >) :
+            (<div> <Box
+                style={{
+                    height: "22em"
+                }}>.</Box></div >) :
             (<div>
                 <h2
                     style={{
@@ -49,53 +53,60 @@ const OutputMovieGenres = ({ genre }) => {
                 <Grid
                     container spacing={2}
                     style={{
-
+                        display: "flex",
                         justifyContent: "center",
                         alignItems: "center",
                     }}
                 >
                     <Grid
                         style={{
-                            justifyContent: "space-between",
-                            display: "flex"
+                            display: "flex",
                         }}
                         item xs={1}>
                         <IconButton
                             onClick={moveLeft}
                             style={{
-                                width: "30px",
-                                height: "60px"
-
+                                width: "100%",
+                                height: "100%",
                             }}>
                             <ArrowBackIosIcon
                                 style={{
-                                    width: "100%",
+                                    width: "40%",
                                     height: "100%",
                                     color: "red",
-                                    scale: "4.0",
-                                    marginLeft: "80px",
+                                    scale: "2.0",
+                                    marginLeft: "1.5em"
                                 }} />
                         </IconButton>
                     </Grid>
                     {movies.map((movie) => (
-                        <Grid item xs key={movie.id} >
+                        <Grid item xs={2} key={movie.id} >
                             <Movie
                                 id={movie.id}
                                 medium_cover_image={movie.medium_cover_image}
                                 title={movie.title}
                                 summary={movie.summary}
                                 genres={movie.genres}
-                                style={{ transform: 'scale(1.0)' }}
                             />
                         </Grid>
                     ))}
-                    <Grid item xs={1}>
-                        <IconButton onClick={moveRight}>
+                    <Grid
+
+                        item xs={1}>
+                        <IconButton
+                            onClick={moveRight}
+                            style={{
+                                width: "100%",
+                                height: "100%"
+                            }}
+                        >
                             <ArrowForwardIosIcon
                                 style={{
+                                    width: "40%",
+                                    height: "100%",
                                     color: "red",
-                                    scale: "4.0",
-                                    marginLeft: "40px",
+                                    scale: "2.0",
+                                    marginRight: "1.5em"
                                 }} />
                         </IconButton>
                     </Grid>
