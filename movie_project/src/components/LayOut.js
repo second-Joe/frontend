151e71355dde7c091e317053982c9c476a70361a
Hoooniya <gnsdl7274@gmail.com>
2023-03-10 19:17:56 +0900

    style: 검색양이 적을 때 footer가 올라가는 오류 수정

--- movie_project/src/components/LayOut.js
+++ movie_project/src/components/LayOut.js
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchResult from "../pages/SearchResult";
 import DownLoadmany from "./DownLoadmany";
 import GoodMany from "./GoodMany";
@@ -12,47 +12,61 @@ const Layout = () => {
   const [menuClick, setMenuClick] = useState(false);
   const [menuKind, setMenuKind] = useState("");
 
+  useEffect(() => {
+    const handleResize = () => {
+      const main = document.querySelector("main");
+      const footer = document.querySelector("footer");
+
+      main.style.minHeight = `calc(100vh - ${header.offsetHeight}px - ${footer.offsetHeight}px)`;
+    };
+
+    const header = document.querySelector("header");
+    const footer = document.querySelector("footer");
+    const main = document.querySelector("main");
+
+    main.style.minHeight = `calc(100vh - ${header.offsetHeight}px - ${footer.offsetHeight}px)`;
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const menuName = menuKind;
-  //console.log(menuName);
+
   return (
-    <div
-      style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
-    >
-      {/* search input에서 글자 입력하면  main 안 section1, section2, section3도 보여주면서 검색결과 바뀌어서 보여줌(section3), 엔터치면 section3으로 전체 화면 대체 */}
-      <div>
-        <header>
-          <NavBar
-            search={search}
-            setSearch={setSearch}
-            menuClick={menuClick}
-            setMenuClick={setMenuClick}
-            menuKind={menuKind}
-            setMenuKind={setMenuKind}
-          />
-        </header>
-        <main>
-          {menuClick === false ? (
-            search === "" ? (
-              <div>
-                <Outlet />
-              </div>
-            ) : (
-              <SearchResult search={search} />
-            )
-          ) : search === "" ? (
-            menuName === "다운로드 가장 많은 영화" ? (
-              <DownLoadmany />
-            ) : menuName === "좋아요 가장 많은 영화" ? (
-              <GoodMany />
-            ) : (
-              <NewDateAdd />
-            )
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <header>
+        <NavBar
+          search={search}
+          setSearch={setSearch}
+          menuClick={menuClick}
+          setMenuClick={setMenuClick}
+          menuKind={menuKind}
+          setMenuKind={setMenuKind}
+        />
+      </header>
+      <main style={{ flex: 1, display: "flex", flexDirection: "column", minHeight: "calc(100vh - 110px)" }}>
+        {menuClick === false ? (
+          search === "" ? (
+            <div>
+              <Outlet />
+            </div>
           ) : (
             <SearchResult search={search} />
-          )}
-        </main>
-        <Footer />
-      </div>
+          )
+        ) : search === "" ? (
+          menuName === "다운로드 가장 많은 영화" ? (
+            <DownLoadmany />
+          ) : menuName === "좋아요 가장 많은 영화" ? (
+            <GoodMany />
+          ) : (
+            <NewDateAdd />
+          )
+        ) : (
+          <SearchResult search={search} />
+        )}
+      </main>
+      <Footer />
     </div>
   );
 };
