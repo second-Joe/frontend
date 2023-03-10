d2fd8091358e559b76aa78164d8b4f17fe149380
diana815 <diana815@naver.com>
2023-03-10 22:29:42 +0900

    fix : homepage 화살표 버튼 클릭시 rerender가 많이 되는 문제 개선

--- movie_project/src/components/OutputMovieGenres.js
+++ movie_project/src/components/OutputMovieGenres.js
@@ -1,120 +1,151 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback, useLayoutEffect } from "react";
 import Grid from "@mui/material/Grid";
 import Movie from "../components/Movie";
-import IconButton from '@mui/material/IconButton';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { Box } from "@mui/material";
 
 const OutputMovieGenres = ({ genre }) => {
-    const [movies, setMovies] = useState([]);
-    const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [moviesIndex, setMoviesIndex] = useState(0);
 
-    useEffect(() => {
-        setLoading(true);
-        loadMovies(page);
-    }, [page]);
+  useLayoutEffect(() => {
+    setLoading(true);
+    loadMovies();
+  }, []);
 
-    const moveLeft = () => {
-        setPage((prevPage) => {
-            return prevPage === 1 ? prevPage + 4 : prevPage - 1;
-        });
-    };
-    const moveRight = () => {
-        setPage((prevPage) => {
-            return prevPage === 5 ? prevPage - 4 : prevPage + 1;
-        });
-    };
+  const moveLeft = useCallback(() => {
+    setMoviesIndex((moviesIndex) => {
+      return moviesIndex === 0 ? moviesIndex + 20 : moviesIndex - 5;
+    });
+  }, []);
+  const moveRight = useCallback(() => {
+    setMoviesIndex((moviesIndex) => {
+      return moviesIndex === 20 ? moviesIndex - 20 : moviesIndex + 5;
+    });
+  }, []);
 
-    const loadMovies = async (page) => {
-        const movieData = await (
-            await fetch(`https://yts.mx/api/v2/list_movies.json?limit=5&genre=${genre}&minimum_rating=8&page=${page}`)
-        ).json();
-        setLoading(false);
-        setMovies(movieData.data.movies);
-    };
+  const loadMovies = async () => {
+    const movieData = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?limit=25&genre=${genre}&minimum_rating=8`
+      )
+    ).json();
 
-    return (<div>
-        {loading ?
-            (<div> <Box
+    let data = movieData.data.movies;
+    setLoading(false);
+    setMovies([data]);
+  };
+
+  const movieRender = () => {
+    const movieList = movies[0];
+    let result = [];
+    for (let i = moviesIndex; i < moviesIndex + 5; i++) {
+      let movie = movieList[i];
+
+      result.push(
+        <Grid item xs={2} key={movie.id}>
+          <Movie
+            id={movie.id}
+            medium_cover_image={movie.medium_cover_image}
+            title={movie.title}
+            summary={movie.summary}
+            genres={movie.genres}
+          />
+        </Grid>
+      );
+    }
+    return result;
+  };
+
+  return (
+    <div>
+      {loading ? (
+        <div>
+          <Box
+            style={{
+              height: "22em",
+            }}
+          >
+            &nbsp;
+          </Box>
+        </div>
+      ) : (
+        <div>
+          <h2
+            style={{
+              marginTop: "0px",
+              justifyContent: "center",
+              marginLeft: "8.5%",
+              color: "white",
+              fontSize: "1.5em",
+            }}
+          >
+            genre : {genre}
+          </h2>
+          <Grid
+            container
+            spacing={2}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Grid
+              style={{
+                display: "flex",
+              }}
+              item
+              xs={1}
+            >
+              <IconButton
+                onClick={moveLeft}
                 style={{
-                    height: "22em"
-                }}>&nbsp;</Box></div >) :
-            (<div>
-                <h2
-                    style={{
-                        marginTop: "0px",
-                        justifyContent: "center",
-                        marginLeft: "8.5%",
-                        color: "white",
-                        fontSize: "1.5em",
-                    }}>genre : {genre}</h2>
-                <Grid
-                    container spacing={2}
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <Grid
-                        style={{
-                            display: "flex",
-                        }}
-                        item xs={1}>
-                        <IconButton
-                            onClick={moveLeft}
-                            style={{
-                                width: "100%",
-                                height: "100%",
-                            }}>
-                            <ArrowBackIosIcon
-                                style={{
-                                    width: "40%",
-                                    height: "100%",
-                                    color: "red",
-                                    scale: "2.0",
-                                    marginLeft: "1.5em"
-                                }} />
-                        </IconButton>
-                    </Grid>
-                    {movies.map((movie) => (
-                        <Grid item xs={2} key={movie.id} >
-                            <Movie
-                                id={movie.id}
-                                medium_cover_image={movie.medium_cover_image}
-                                title={movie.title}
-                                summary={movie.summary}
-                                genres={movie.genres}
-                            />
-                        </Grid>
-                    ))}
-                    <Grid
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                <ArrowBackIosIcon
+                  style={{
+                    width: "40%",
+                    height: "100%",
+                    color: "red",
+                    scale: "2.0",
+                    marginLeft: "1.5em",
+                  }}
+                />
+              </IconButton>
+            </Grid>
 
-                        item xs={1}>
-                        <IconButton
-                            onClick={moveRight}
-                            style={{
-                                width: "100%",
-                                height: "100%"
-                            }}
-                        >
-                            <ArrowForwardIosIcon
-                                style={{
-                                    width: "40%",
-                                    height: "100%",
-                                    color: "red",
-                                    scale: "2.0",
-                                    marginRight: "1.5em"
-                                }} />
-                        </IconButton>
-                    </Grid>
-                </Grid>
-            </div>)
-        }
+            {movieRender()}
+
+            <Grid item xs={1}>
+              <IconButton
+                onClick={moveRight}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                <ArrowForwardIosIcon
+                  style={{
+                    width: "40%",
+                    height: "100%",
+                    color: "red",
+                    scale: "2.0",
+                    marginRight: "1.5em",
+                  }}
+                />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </div>
+      )}
     </div>
-    );
-}
+  );
+};
 
 export default OutputMovieGenres;
