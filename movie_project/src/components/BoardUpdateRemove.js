d679b0ea3dcbd595a640bc20aa1ab6e4a2608a03
diana815 <diana815@naver.com>
2023-03-26 16:48:54 +0900

    feat : 게시판 title을 클릭했을 때 수정/삭제 할 수 있는 button, input created

---
+++ movie_project/src/components/BoardUpdateRemove.js
@@ -0,0 +1,127 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Typography from "@mui/material/Typography";
+import { useSpring, animated } from "@react-spring/web";
+import OutlinedTextField from "./OutlinedTextField";
+import CustomizedButton from "./CustomizedButton";
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const {
+    children,
+    in: open,
+    onClick,
+    onEnter,
+    onExited,
+    ownerState,
+    ...other
+  } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter(null, true);
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited(null, true);
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {React.cloneElement(children, { onClick })}
+    </animated.div>
+  );
+});
+
+Fade.propTypes = {
+  children: PropTypes.element.isRequired,
+  in: PropTypes.bool,
+  onClick: PropTypes.any,
+  onEnter: PropTypes.func,
+  onExited: PropTypes.func,
+  ownerState: PropTypes.any,
+};
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  color: "white",
+  width: 560,
+  bgcolor: "black",
+  border: "1px solid white",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function PasswordCheck({ openModal, handleOpen, handleClose }) {
+  const [open, setOpen] = React.useState(openModal);
+
+  const handleClose2 = () => {
+    setOpen(false);
+    handleClose();
+  };
+
+  const handleUpdate = () => {};
+  const handleRemove = () => {};
+  return (
+    <div>
+      <Modal
+        aria-labelledby="spring-modal-title"
+        aria-describedby="spring-modal-description"
+        open={open}
+        onClose={handleClose2}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            TransitionComponent: Fade,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography
+              sx={{ width: 160, mx: "auto", mb: 3 }}
+              id="spring-modal-title"
+              variant="h5"
+              component="h2"
+            >
+              게시판 수정 및 삭제하기
+            </Typography>
+            <Box sx={{ display: "flex" }}>
+              <Typography
+                sx={{ width: "150px", mr: 5, mt: 3 }}
+                variant="h10"
+                component="h4"
+              >
+                비밀번호 입력
+              </Typography>
+              <OutlinedTextField label="게시판 비밀번호를 입력해주세요" />
+            </Box>
+            <Box sx={{ mx: "auto", width: 50 }}>
+              <CustomizedButton
+                label="수정"
+                value="update"
+                onClick={handleUpdate}
+              ></CustomizedButton>
+              <CustomizedButton
+                label="삭제"
+                value="remove"
+                onClick={handleRemove}
+              ></CustomizedButton>
+            </Box>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
