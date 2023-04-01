8f806f75f91396aabafc133efb0b2a82f4097a8d
diana815 <diana815@naver.com>
2023-04-01 17:44:04 +0900

    fix : mypage반응형으로 코드 수정

--- movie_project/src/components/MyPageBody.js
+++ movie_project/src/components/MyPageBody.js
@@ -12,14 +12,38 @@ import EmailChange from "./EmailChange";
 import PasswordChange from "./PasswordChange";
 import PhoneChange from "./PhoneChange";
 import axios from "axios";
+import { useMediaQuery, useTheme } from "@mui/material";
 
 const MyPageBody = () => {
+  const theme = useTheme();
   const [passwordSearch, setPasswordSearch] = useState(false);
   const [email, setEmail] = useState(window.sessionStorage.getItem("id"));
   const [tel, setTel] = useState("");
   const [pw, setPw] = useState("");
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMiddleScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const [user, setUser] = React.useState(window.sessionStorage.getItem("id"));
+  const [userName, setUserName] = React.useState("");
 
   const navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .post("/selectMember", {
+        member_id: user,
+      })
+      .then((res) => {
+        console.log("selectMember =>", res);
+        if (res.data !== null) {
+          setUserName(res.data.member_name);
+          // alert("정보 확인 성공!");
+        } else {
+          alert("정보 확인 실패!");
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }, []);
   useEffect(() => {
     axios
       .post("/selectMember", {
@@ -84,17 +108,33 @@ const MyPageBody = () => {
         console.error(e);
       });
   };
+
   return (
     <Container sx={{ paddingTop: "100px" }}>
       <Typography sx={{ fontSize: 35, mb: 2 }}>계정</Typography>
       <Divider />
       <Grid container sx={{ m: 1 }}>
-        <Grid item xs={3} sx={{ fontSize: 19, color: "gray" }}>
-          멤버십 & 결제 정보
-        </Grid>
+        {isSmallScreen ? (
+          <Grid
+            item
+            xs={12}
+            sx={{ fontSize: 19, color: "black", mb: 3, fontWeight: "bold" }}
+          >
+            멤버십 & 결제 정보
+          </Grid>
+        ) : (
+          <Grid
+            item
+            xs={3}
+            sx={{ fontSize: 19, color: "black", fontWeight: "bold" }}
+          >
+            멤버십 & 결제 정보
+          </Grid>
+        )}
+
         <Grid item xs={9} sx={{ direction: "column" }}>
           <Grid container>
-            <Grid item xs={8} sx={{ pl: 2, pt: 3, color: "gray" }}>
+            <Grid item xs={9} sx={{ pl: 2, pt: 3, color: "gray" }}>
               이메일 주소 : {email}
             </Grid>
             <Grid item xs={2} sx={{ p: 2 }}>
@@ -116,7 +156,7 @@ const MyPageBody = () => {
                 ) : null}
               </Box>
             </Grid>
-            <Grid item xs={8} sx={{ pl: 2, pt: 3, color: "gray" }}>
+            <Grid item xs={9} sx={{ pl: 2, pt: 3, color: "gray" }}>
               비밀번호 : {pw}
             </Grid>
             <Grid item xs={2} sx={{ p: 2 }}>
@@ -139,10 +179,10 @@ const MyPageBody = () => {
                 ) : null}
               </Box>
             </Grid>
-            <Grid item xs={8} sx={{ pl: 2, pt: 3, color: "gray" }}>
+            <Grid item xs={9} sx={{ pl: 2, pt: 3, color: "gray" }}>
               휴대폰 번호 : {tel}
             </Grid>
-            <Grid item xs={3} sx={{ p: 2 }}>
+            <Grid item xs={2} sx={{ p: 2 }}>
               <Box sx={{ width: 200, mb: 2 }}>
                 <CustomizedButton
                   label="휴대폰 번호 등록"
@@ -175,17 +215,38 @@ const MyPageBody = () => {
       <Divider />
 
       <Grid container sx={{ m: 1 }}>
-        <Grid item xs={3} sx={{ fontSize: 19, color: "gray" }}>
-          멤버십 상세 정보
-        </Grid>
+        {isSmallScreen ? (
+          <Grid
+            item
+            xs={12}
+            sx={{ fontSize: 19, color: "black", mb: 3, fontWeight: "bold" }}
+          >
+            멤버십 상세 정보
+          </Grid>
+        ) : (
+          <Grid
+            item
+            xs={3}
+            sx={{ fontSize: 19, color: "black", fontWeight: "bold" }}
+          >
+            멤버십 상세 정보
+          </Grid>
+        )}
+
         <Grid item xs={9} sx={{ direction: "column" }}>
           <Grid container>
             <Grid item xs={9} sx={{ p: 2 }}>
               스트리밍 멤버십에 가입하지 않으셨습니다.
             </Grid>
-            <Grid item xs={3} sx={{ p: 2 }}>
+            <Grid item xs={3} sx={{ pr: 2 }}>
               <Typography
-                sx={{ fontSize: 14, mb: 3, color: "blue", textAlign: "right" }}
+                sx={{
+                  fontSize: 14,
+                  mr: 5,
+                  mb: 3,
+                  color: "blue",
+                  textAlign: "right",
+                }}
               >
                 스트리밍 멤버십 추가
               </Typography>
@@ -197,15 +258,28 @@ const MyPageBody = () => {
       <Divider />
 
       <Grid container sx={{ m: 1 }}>
-        <Grid item xs={3} sx={{ fontSize: 19, color: "gray" }}>
-          프로필 & 자녀 보호 설정
-        </Grid>
-        <Grid item xs={9} sx={{ direction: "column" }}>
+        {isSmallScreen ? (
+          <Grid
+            item
+            xs={12}
+            sx={{ fontSize: 19, color: "black", mb: 3, fontWeight: "bold" }}
+          >
+            프로필 & 자녀 보호 설정
+          </Grid>
+        ) : (
+          <Grid
+            item
+            xs={3}
+            sx={{ fontSize: 19, color: "black", fontWeight: "bold" }}
+          >
+            프로필 & 자녀 보호 설정
+          </Grid>
+        )}
+
+        <Grid item xs={9} sx={{ direction: "row" }}>
           <Grid container>
-            <Grid item xs={1}>
+            <Box sx={{ display: "flex" }}>
               <AccountBoxIcon sx={{ fontSize: 80 }} />
-            </Grid>
-            <Grid item xs={11}>
               <Typography
                 sx={{
                   pl: 2,
@@ -215,9 +289,9 @@ const MyPageBody = () => {
                   fontWeight: "bold",
                 }}
               >
-                유저 이름
+                {userName}
               </Typography>
-            </Grid>
+            </Box>
             <Grid></Grid>
           </Grid>
         </Grid>
