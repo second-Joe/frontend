521afcd5ca3b6ce8fa3b79bd39a0eeb30da8bc7c
diana815 <diana815@naver.com>
2023-04-03 20:16:27 +0900

    fix : myPage navbar 반응형으로 만들기

--- movie_project/src/components/MyPageNavbar.js
+++ movie_project/src/components/MyPageNavbar.js
@@ -8,89 +8,51 @@ import MyPageDropdown from "../components/MyPageDropdown";
 
 const StickyHeader = () => {
   const theme = useTheme();
+  const isxlargescreen = useMediaQuery(theme.breakpoints.down("xl"));
+  const isLargescreen = useMediaQuery(theme.breakpoints.down("lg"));
   const isMiddleScreen = useMediaQuery(theme.breakpoints.down("md"));
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isxSmallscreen = useMediaQuery(theme.breakpoints.down("xs"));
   return (
-    <AppBar position="fixed">
+    <AppBar
+      position="fixed"
+      style={{
+        background: "#212121",
+        width: "100%",
+      }}
+    >
       <Toolbar
         style={{
           height: 70,
           background: "#212121",
+          width: "100%",
         }}
       >
-        {isSmallScreen ? (
-          <Grid container>
-            <Grid item xs={3}>
-              <Typography
-                noWrap
-                component="a"
-                href="/login"
-                sx={{
-                  mr: 2,
-                  fontFamily: "helvetica",
-                  fontWeight: 1000,
-                  letterSpacing: ".1rem",
-                  color: "red",
-                  textDecoration: "none",
-                  fontSize: "2.5em",
-                }}
-              >
-                {isSmallScreen ? "N" : "NETFLIX"}
-              </Typography>
-            </Grid>
-            <Grid item xs={9}>
-              <MyPageDropdown />
-            </Grid>
-          </Grid>
-        ) : isMiddleScreen ? (
-          <Grid container>
-            <Grid item xs={6}>
-              <Typography
-                noWrap
-                component="a"
-                href="/login"
-                sx={{
-                  mr: 2,
-                  fontFamily: "helvetica",
-                  fontWeight: 1000,
-                  letterSpacing: ".1rem",
-                  color: "red",
-                  textDecoration: "none",
-                  fontSize: "2.5em",
-                }}
-              >
-                {isSmallScreen ? "N" : "NETFLIX"}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <MyPageDropdown />
-            </Grid>
-          </Grid>
-        ) : (
-          <Grid container>
-            <Grid item xs={9}>
-              <Typography
-                noWrap
-                component="a"
-                href="/login"
-                sx={{
-                  mr: 2,
-                  fontFamily: "helvetica",
-                  fontWeight: 1000,
-                  letterSpacing: ".1rem",
-                  color: "red",
-                  textDecoration: "none",
-                  fontSize: "2.5em",
-                }}
-              >
-                {isSmallScreen ? "N" : "NETFLIX"}
-              </Typography>
-            </Grid>
-            <Grid item xs={2}>
-              <MyPageDropdown />
-            </Grid>
-          </Grid>
-        )}
+        <Box container sx={{ display: "flex", width: "100%" }}>
+          <Box sx={{ width: "20%" }}>
+            <Typography
+              noWrap
+              component="a"
+              href="/login"
+              sx={{
+                mr: 2,
+                fontFamily: "helvetica",
+                fontWeight: 1000,
+                letterSpacing: ".1rem",
+                color: "red",
+                textDecoration: "none",
+                fontSize: "2.5em",
+              }}
+            >
+              {isSmallScreen ? "N" : "NETFLIX"}
+            </Typography>
+          </Box>
+          <Box
+            sx={{ width: "80%", display: "flex", justifyContent: "end", mr: 3 }}
+          >
+            <MyPageDropdown />
+          </Box>
+        </Box>
       </Toolbar>
     </AppBar>
   );
