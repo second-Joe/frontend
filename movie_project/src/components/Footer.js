047a661081829083ebcd9521971da4bc4c82faf0
dbtmdtt <dbtmdtt@gmail.com>
2023-04-04 17:28:33 +0900

    footer 조원 수정, 지금가입하세요 테두리 변경

--- movie_project/src/components/Footer.js
+++ movie_project/src/components/Footer.js
@@ -38,9 +38,9 @@ const Footer = () => {
           >
             <li style={{ margin: "0 1.5rem" }}>2조</li>
             <li style={{ margin: "0 1.5rem" }}>김다희</li>
-            <li style={{ margin: "0 1.5rem" }}>김병우</li>
+
             <li style={{ margin: "0 1.5rem" }}>김지훈</li>
-            <li style={{ margin: "0 1.5rem" }}>남승인</li>
+
             <li style={{ margin: "0 1.5rem" }}>이길주</li>
             <li style={{ margin: "0 1.5rem" }}>전유승</li>
             {showButton && (
